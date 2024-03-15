7916474cbe053ede0c3d92574f291964f131d270
Phuc Proton <phuc.le.it96@gmail.com>
2024-03-16 02:13:44 +0700

    Adding description and restructure the layout

--- main.py
+++ main.py
@@ -17,14 +17,20 @@ with col2:
         "**Below you can find some of the apps I have built in Python and Source Code in Github, feel free to contact "
         "me!**")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv('data.csv', sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        image_path = f'images/{index+1}.png'
+        st.image(image=image_path, width=500)
+        st.markdown(f"<a style=\"color:red;\" href=\"{row['url']}\"><b>SOURCE CODE</b></a>", unsafe_allow_html=True)
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
-
+        st.write(row['description'])
+        st.image("images/"+row['image'], width=400)
+        st.markdown(f"<a style=\"color:red;\" href=\"{row['url']}\"><b>SOURCE CODE</b></a>", unsafe_allow_html=True)
