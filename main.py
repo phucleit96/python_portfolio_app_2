e78085ee45cf7b529fc89868b5127eee347b2648
Phuc Proton <phuc.le.it96@gmail.com>
2024-03-16 00:26:50 +0700

    Add description

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ with col1:
 with col2:
     st.title('Phuc Le')
     content = """
-    Hello, I'm Phuc Le, a VNese data enthusiast with 2 years of self-taught experience under my belt, I'm passionate about using Python and its frameworks (Django, Flask, FastAPI) to solve problems (web development, data automation, data science, ...). My skillset also extends to Figma, Powerpoint, Canva, SQL and PowerBI, HTML, CSS, JS. With a strong desire to learn, tackle challenges and record the process, I hope to become a valuable asset in any data-driven environment and play a pivotal role in any team.
+    Hello, I'm Phuc Le, a Vietnamese data enthusiast with 1.5 years of self-taught experience under my belt, I'm passionate about using Python and its frameworks (Django, Flask, FastAPI) to solve problems (web development, data automation, data science, ...). My skillset also extends to Figma, Powerpoint, Canva, SQL and PowerBI, HTML, CSS, JS. With a strong desire to learn, tackle challenges and record the process, I hope to become a valuable asset in any data-driven environment and play a pivotal role in any team.
     """
     st.info(content)
+
+st.write("Below you can find some of the apps I have built in Python and Source Code in Github, feel free to contact "
+         "me!")
