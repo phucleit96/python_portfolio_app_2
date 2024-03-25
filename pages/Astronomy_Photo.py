36356cde10f40442dffef269e1cc4c54c24c7823
Phuc Proton <phuc.le.it96@gmail.com>
2024-03-25 15:09:19 +0700

    fix bug Astronomy Photo

--- pages/Astronomy_Photo.py
+++ pages/Astronomy_Photo.py
@@ -8,7 +8,21 @@ response = requests.get(url)
 content = response.json()
 
 st.title("ASTRONOMY PICTURE OF THE DAY!!!")
-st.markdown(f"<h1 style='text-align: center;'>{content['title']}</h1>", unsafe_allow_html=True)
-st.image(image=content['url'])
-st.write(content['explanation'])
-st.markdown(f"<p style='text-align: right;'><i>{content['copyright']}</i></p>", unsafe_allow_html=True)
+
+if 'title' in content:
+    st.markdown(f"<h1 style='text-align: center;'>{content['title']}</h1>", unsafe_allow_html=True)
+
+# Check if the 'url' key exists and if the URL is a YouTube URL
+if 'url' in content and 'youtube' in content['url']:
+    # Display the video
+    st.video(content['url'])
+elif 'url' in content:
+    # Display the image
+    st.image(image=content['url'])
+
+if 'explanation' in content:
+    st.write(content['explanation'])
+
+# Check if 'copyright' key exists in the content
+if 'copyright' in content:
+    st.markdown(f"<p style='text-align: right;'><i>{content['copyright']}</i></p>", unsafe_allow_html=True)
